b93db956154603a8174547dd2fbea0334778e1e5
derrek-gass <dardcore@gmail.com>
2021-01-25 20:52:22 -0800

    WIP - beginning search funcionality implementation - working out request query formation

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -8,7 +8,7 @@ const querystring = require('querystring');
 const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = require('../config');
 
 
-router.get('/token', cors(), async function (req, res, next) {
+router.get('/token', cors(), async function(req, res, next) {
     try {
         let tokenReq = await axios({
             method: 'post',
@@ -29,6 +29,24 @@ router.get('/token', cors(), async function (req, res, next) {
         return res.json(token);
     }
     catch (err) {
+        return next(err);
+    }
+});
+
+router.get('/search', cors(), async function(req, res, next) {
+    try {
+        let searchReq = await axios({
+            method: 'get',
+            url: 'https://api.spotify.com/v1/search',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: 'BQDRHhNEtqy_VIFBPdXVdpZ2iiXjGZW4um87YXLg_hx7vkis9Ng5STnmsgkTPlwatIj1w9RWeXF1IgQebAI'
+            }
+        });
+
+        return res.json(searchReq);
+    }
+    catch (err){
         return next(err);
     }
 });
